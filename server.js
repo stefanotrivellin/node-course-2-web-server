3661761f9ffa1e1eff5d52cf3202f4f789a64fb8
Trive-VAIO\Trive <triventrive@gmail.com>
2018-01-13 18:30:33 +0100

    Page title variable corrected

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/bad', (req, res) => {
 
 app.get('/projects', (req, res) => {
   res.render('projects.hbs', {
-    pageTitle: 'Home Page',
+    pageTitle: 'Projects Page',
     projectsMessage: 'Portfolio projects here.'
   });
 });
