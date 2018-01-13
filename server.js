213b6cec409cb48c1a35a455af346156945d56a1
Trive-VAIO\Trive <triventrive@gmail.com>
2018-01-13 18:28:55 +0100

    Page title variable added to projects page

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ app.get('/bad', (req, res) => {
 
 app.get('/projects', (req, res) => {
   res.render('projects.hbs', {
+    pageTitle: 'Home Page',
     projectsMessage: 'Portfolio projects here.'
   });
 });
